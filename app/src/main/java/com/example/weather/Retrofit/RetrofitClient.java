bb9b94a7bddb569f6f728442b294fca9101b7c84
mohammed-elhaddouchi <mohammed.elhaddouchi@nepgroup.com>
2020-01-12 18:48:38 +0100

    some improvements.

--- app/src/main/java/com/example/weather/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/weather/Retrofit/RetrofitClient.java
@@ -1,7 +1,5 @@
 package com.example.weather.Retrofit;
 
-import android.util.Log;
-
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
